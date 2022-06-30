f528da6399f85404db00d83be1bd232dd60e1df7
Imran <smdimran838@gmail.com>
2022-07-01 02:09:18 +0530

    added starblock variables

--- com/aidev/starblockchain/StarBlock.java
+++ com/aidev/starblockchain/StarBlock.java
@@ -1,5 +1,22 @@
 package com.aidev.starblockchain;
 
 public class StarBlock {
-    
+
+    private long horizontalIndex;
+    private long verticalIndex;        
+    private long creationTimeStamp;
+    private String currentHash;
+    private String horizontalPreviousHash_relatedFrom;
+    private String horizontalNextHash_relatedTo;
+    private String verticalPreviousHash_unrelatedFrom;
+    private String verticalNextHash_unrelatedTo;
+    private long defaultVersionNumberRelated;
+    private long currentVersionNumberRelated;    
+    private long latestVersionNumberRelated;
+    private long defaultVersionNumberUnrelated;
+    private long currentVersionNumberUnrelated;    
+    private long latestVersionNumberUnrelated;        
+    private long totalTensorNetworkStrength;    
+    private String dataStoreImmutable;
+    private int nonceUserValue;
 }
